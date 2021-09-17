8bd53cbbd43d2e2ca61269922d33d6eb73ad024f
kuba58426 <kuba23031999@gmail.com>
2021-09-17 17:48:16 +0200

    Git, GitHub CLI
    Signed-off-by: kuba58426 <kuba23031999@gmail.com>

--- quickstart_windows_extended.ps1
+++ quickstart_windows_extended.ps1
@@ -12,7 +12,7 @@ wget "https://download.brother.com/welcome/dlf004452/dcp-j315w-inst-B1-cd5.EXE" 
 
 # Minimum programów ----
 choco install googlechrome --ignore-checksums -y
-choco install cmder grep sed awk jre8 adobereader 7zip irfanview irfanviewplugins k-litecodecpack-standard driverbooster avastfreeantivirus kate utorrent steam-client python r r.studio jamovi kdeconnect-kde -y
+choco install cmder grep sed awk jre8 adobereader 7zip irfanview irfanviewplugins k-litecodecpack-standard driverbooster avastfreeantivirus kate utorrent steam-client python r r.studio jamovi kdeconnect-kde git gh -y
 choco install office2019proplus --params '/Language:pl-pl' -y
 refreshenv
 
