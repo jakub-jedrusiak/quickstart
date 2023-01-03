ae6e6c6bee7510be65a0f3832485a0cd5e5f44b2
kuba58426 <kuba23031999@gmail.com>
2023-01-03 17:33:25 +0100

    quickstart_R

--- quickstart_R.R
+++ quickstart_R.R
@@ -1,2 +1,14 @@
-pacman::p_load(rstatix, tidyverse, magrittr, lubridate, onewaytests, psych)
-#pacman::p_load(DBI, RMariaDB, dbplyr)
+install.packages("pacman")
+pacman::p_install(
+    tidyverse, magrittr, rstatix, psych, # data wrangling
+    caret, lavaan, ranger, ClusterR, modelbased, robust, # modelling
+    httpgd, lintr, languageserver, # vcode
+    DBI, dbplyr, RMariaDB, # database utilities
+    broom, lm.beta, # model exploration
+    knitr, kableExtra, learnr, rmarkdown, shiny, # reporting
+    ggthemes, # plotting
+    roxygen2, testthat, devtools, rlang, # programming
+    force = FALSE
+    )
+devtools::install_github("crsh/papaja")
+devtools::install_github("jakub-jedrusiak/jedrusiakr")
